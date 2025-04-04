ad5cdee5f02f2d5a9153e872661d2d634eed0090
gustavo <mifmobng@gmail.com>
2025-04-04 08:55:59 -0300

    oi

--- Comparacao.java
+++ Comparacao.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Random;
 
 public class Comparacao {
@@ -25,81 +24,51 @@ public class Comparacao {
 
     static void testeSS(NossoVetor vetor, int reps, String watermark){
         long[] total = new long[reps];
-        long[] time = new long[reps];
         for(int i = 0; i < reps; i++){
             vetor.preecheVetor();
-            long ini = Calendar.getInstance().getTimeInMillis();
             long trocas = vetor.selectionSort();
-            long fim = Calendar.getInstance().getTimeInMillis();
-            long tempo = fim-ini;
             total[i] = trocas;
-            time[i] = tempo;
         }
         System.out.println(watermark + Arrays.toString(total));
-        System.out.println(watermark + Arrays.toString(time));
     }
 
     static void testeIS(NossoVetor vetor, int reps, String watermark){
         long[] total = new long[reps];
-        long[] time = new long[reps];
         for(int i = 0; i < reps; i++){
             vetor.preecheVetor();
-            long ini = Calendar.getInstance().getTimeInMillis();
             long trocas = vetor.insertionSort();
-            long fim = Calendar.getInstance().getTimeInMillis();
-            long tempo = fim-ini;
             total[i] = trocas;
-            time[i] = tempo;
         }
         System.out.println(watermark + Arrays.toString(total));
-        System.out.println(watermark + Arrays.toString(time));
     }
 
     static void testeBBS(NossoVetor vetor, int reps, String watermark) {
         long[] total = new long[reps];
-        long[] time = new long[reps];
         for(int i = 0; i < reps; i++){
             vetor.preecheVetor();
-            long ini = Calendar.getInstance().getTimeInMillis();
             long trocas = vetor.bubbleSort();
-            long fim = Calendar.getInstance().getTimeInMillis();
-            long tempo = fim-ini;
             total[i] = trocas;
-            time[i] = tempo;
         }
         System.out.println(watermark + Arrays.toString(total));
-        System.out.println(watermark + Arrays.toString(time));
     }
 
     static void comparacaoBuscas(NossoVetor vetor, int reps, String watermark){
         long[] totalLinear = new long[reps];
-        long[] timeLinear = new long[reps];
         long[] totalBinario = new long[reps];
-        long[] timeBinario = new long[reps];
         for(int i = 0; i < reps; i++){
             vetor.preecheVetor();
             Random random = new Random();
             long alvo = random.nextInt(vetor.getTamanho());
-            long ini = Calendar.getInstance().getTimeInMillis();
             long comp = vetor.buscaSimples(alvo);
-            long fim = Calendar.getInstance().getTimeInMillis();
-            long tempo = fim - ini;
             totalLinear[i] = comp;
-            timeLinear[i] = tempo;
 
-            ini = Calendar.getInstance().getTimeInMillis();
             //ordenar
             vetor.insertionSort();
             //buscar
             comp = vetor.buscaBinaria(alvo);
-            fim = Calendar.getInstance().getTimeInMillis();
-            tempo = fim - ini;
             totalBinario[i] = comp;
-            timeBinario[i] = tempo;
         }
         System.out.println(watermark + Arrays.toString(totalLinear));
-        System.out.println(watermark + Arrays.toString(timeLinear));
         System.out.println(watermark + Arrays.toString(totalBinario));
-        System.out.println(watermark + Arrays.toString(timeBinario)+ "\n");
     }
 }
--- graficos.py
+++ graficos.py
@@ -1,27 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import ast
+import statistics
+
+
 with open('resultados.txt', 'r') as arquivo:
     read_data = arquivo.readlines()
+
+v100k = []
+v200k = []
+v400k = []
+v800k = []
+v1m6k = []
 
 def criacao_listas():
     for i in range(len(read_data)):
         x = read_data[i][0:4]
         match x:
             case "100k":
-                print("oi")
+                v100k.append(ast.literal_eval(read_data[i][4:]))
             case "200k":
-                print("dois")
+                v200k.append(ast.literal_eval(read_data[i][4:]))
             case "400k":
-                print("tres")
+                v400k.append(ast.literal_eval(read_data[i][4:]))
             case "800k":
-                print("quatro")
+                v800k.append(ast.literal_eval(read_data[i][4:]))
             case "16kk":
-                print("cinco")
+                v1m6k.append(ast.literal_eval(read_data[i][4:]))
             case _:
                 pass
-                
 
 criacao_listas()
 
-
-
-
-
+def criar_graficos(algoritmo):
+    match algoritmo:
+        case "insertionsort":
+            data = [statistics.mean(v100k[1]), statistics.mean(v200k[1]), statistics.mean(v400k[1]), statistics.mean(v800k[1]), statistics.mean(v1m6k[1])]
+            horizontal = ["100k", "200k", "400k", "800k", "1.6M"]
+            xpoints = np.array(horizontal)
+            ypoints = np.array(data)
+            plt.plot(xpoints,ypoints)
+            plt.show()
+        case "selectionsort":
+            data = [statistics.mean(v100k[0]), statistics.mean(v200k[0]), statistics.mean(v400k[0]), statistics.mean(v800k[0]), statistics.mean(v1m6k[0])]
+            horizontal = ["100k", "200k", "400k", "800k", "1.6M"]
+            xpoints = np.array(horizontal)
+            ypoints = np.array(data)
+            plt.plot(xpoints,ypoints)
+            plt.show()
+        case "bubblesort":
+            data = [statistics.mean(v100k[2]), statistics.mean(v200k[2]), statistics.mean(v400k[2]), statistics.mean(v800k[2]), statistics.mean(v1m6k[2])]
+            horizontal = ["100k", "200k", "400k", "800k", "1.6M"]
+            xpoints = np.array(horizontal)
+            ypoints = np.array(data)
+            plt.plot(xpoints,ypoints)
+            plt.show()
+criar_graficos("selectionsort")
+criar_graficos("insertionsort")
+criar_graficos("bubblesort")
